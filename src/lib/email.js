2de64e04ec75c6711a19bf48903b0d0bc40250cf
Kaio <91427326+Kaicius@users.noreply.github.com>
2026-05-12 22:57:08 -0300

    Update email.js

--- src/lib/email.js
+++ src/lib/email.js
@@ -13,9 +13,9 @@ const configOptions = nodemailer.createTransport({
 
 export async function emailReset_Enviado(nome, sobrenome, emailDestinatario, codigo) {
     const emailReset = {
-        from: `"Suporte Gemn Pizzaria" <${process.env.EMAIL_USER}>`,
+        from: `"Suporte Genm Pizzaria" <${process.env.EMAIL_USER}>`,
         to: emailDestinatario,
-        subject:"Reset de Senha - Gemn Pizzaria",
+        subject:"Reset de Senha - Genm Pizzaria",
         html: `
         Olá <strong> ${nome} ${sobrenome}, 
         <br><br> Você solicitou uma redefinição de senha. <br><br> 
@@ -23,7 +23,7 @@ export async function emailReset_Enviado(nome, sobrenome, emailDestinatario, cod
         <br><br> <h3> ${codigo} </h3> <br><br>
         Este codigo expira em 10 minutos
         <br><br> <strong> Caso essa solicitação não seja sua ignore esse email 
-        <br><br> Atenciosamente,<br>Equipe de Suporte Gemn Pizzaria </strong>
+        <br><br> Atenciosamente,<br>Equipe de Suporte Genm Pizzaria </strong>
         `
     }
 
